ced193dbbb4f25fcc63beb10b7ca575b64ec8e23
Purwo Lapiza <purwo.lapiza@mitrais.com>
2017-02-09 17:01:31 +0700

    test1

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,15 +15,6 @@ node {
 		checkout scm	
 	}
 	
-	
-	stage("Sonar Analyze") {
-		def scannerHome = tool 'default';
-	    withSonarQubeEnv('default') {
-	      sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${deploymentGroupName} -Dsonar.sources=app"
-	    }
-	}
-	
-
 	stage('Deploy') {
 		withCredentials([[$class          : 'UsernamePasswordMultiBinding',
                       credentialsId   : 'CdcAWS',
