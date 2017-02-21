8f2fac569e251f8e3bf223fe4c007c90fecec489
Purwo Lapiza <purwo.lapiza@mitrais.com>
2017-02-21 16:50:44 +0700

    Update deployment group and destination folder

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ import groovy.json.JsonSlurperClassic
 node {
     env.AWS_DEFAULT_REGION = 'ap-southeast-1'
 	
-	def applicationName = 'purwo' //change me
+	def applicationName = 'langit-cdc' //change me
 	def deploymentGroupName = 'purwo' // change me
 	def s3BucketName = 'deployment-cdc'
 	
