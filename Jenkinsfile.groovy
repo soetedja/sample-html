f86d116bd720d54b634056a14fb37f158302f243
irfansoetedja <soetedja@gmail.com>
2017-02-22 17:06:38 +0700

    add files

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,42 +3,42 @@ import groovy.json.JsonSlurperClassic
 
 node {
     env.AWS_DEFAULT_REGION = 'ap-southeast-1'
-	
-	def applicationName = 'purwo-cdc' //change me
-	def deploymentGroupName = 'purwo-cdc' // change me
+
+	def applicationName = 'irfan' //change me
+	def deploymentGroupName = 'irfan' // change me
 	def s3BucketName = 'deployment-cdc'
-	
+
 	//Cleanup workspace
 	deleteDir()
 
 	stage('Checkout') {
-		checkout scm	
+		checkout scm
 	}
-	
+
 	stage('Deploy') {
 		withCredentials([[$class          : 'UsernamePasswordMultiBinding',
                       credentialsId   : 'CdcAWS',
                       usernameVariable: 'AWS_ACCESS_KEY_ID',
                       passwordVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-	        
-	        
+
+
 
 	        //Zip artifact
 		def artifactName = "${applicationName}-${deploymentGroupName}"
 	        sh("zip -r ${artifactName}.zip .")
-	        
+
 	        //Upload artifact to S3
 		sh("aws s3 cp ${artifactName}.zip s3://${s3BucketName}/${artifactName}.zip")
 
 	        //Create Deployment
-	        def result = sh(returnStdout:true, script: "aws deploy create-deployment --application-name ${applicationName} --deployment-group-name ${deploymentGroupName} --s3-location bucket=${s3BucketName},bundleType=zip,key=${artifactName}.zip")	        
+	        def result = sh(returnStdout:true, script: "aws deploy create-deployment --application-name ${applicationName} --deployment-group-name ${deploymentGroupName} --s3-location bucket=${s3BucketName},bundleType=zip,key=${artifactName}.zip")
 	        def json = parseJson(result)
 	        String deploymentId = json.deploymentId
 
 	        //Wait until success
 	        sh("aws deploy wait deployment-successful --deployment-id ${deploymentId}")
 	    }
-	}	
+	}
 }
 
 def parseJson(String json) {
