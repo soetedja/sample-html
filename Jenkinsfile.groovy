2a09dce2a56539a5a86062756f3270cb9d1a470c
irfansoetedja <soetedja@gmail.com>
2017-02-22 17:10:55 +0700

    change name app

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ import groovy.json.JsonSlurperClassic
 node {
     env.AWS_DEFAULT_REGION = 'ap-southeast-1'
 
-	def applicationName = 'irfan' //change me
+	def applicationName = 'irfan-app' //change me
 	def deploymentGroupName = 'irfan' // change me
 	def s3BucketName = 'deployment-cdc'
 
